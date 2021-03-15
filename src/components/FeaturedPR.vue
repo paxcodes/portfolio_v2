dc14599be14b86f90e7ec84c6137a61283caeec8
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-15 07:57:38 -0700

    feat: add skeleton of oss section

---
+++ src/components/FeaturedPR.vue
@@ -0,0 +1,68 @@
+<template functional>
+  <div class="pr">
+    <div>
+      <span class="project">
+        <span title="Status: Merged" class="State State--merged">
+          <svg
+            height="16"
+            class="octicon octicon-git-merge"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 3.254V3.25v.005a.75.75 0 110-.005v.004zm.45 1.9a2.25 2.25 0 10-1.95.218v5.256a2.25 2.25 0 101.5 0V7.123A5.735 5.735 0 009.25 9h1.378a2.251 2.251 0 100-1.5H9.25a4.25 4.25 0 01-3.8-2.346zM12.75 9a.75.75 0 100-1.5.75.75 0 000 1.5zm-8.5 4.5a.75.75 0 100-1.5.75.75 0 000 1.5z"
+            ></path>
+          </svg>
+          Merged
+        </span>
+        {{ props.pr.project }} #{{ props.pr.issueNum }} <br
+      /></span>
+      <p class="pr-title">{{ props.pr.title }}</p>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.pr {
+  margin: 30px 0;
+}
+.project {
+  font-weight: bold;
+}
+
+.pr-title {
+  font-style: italic;
+  margin-top: 5px;
+}
+</style>
+
+<style>
+.State--merged {
+  color: #fff;
+  background-color: #6f42c1;
+  display: flex;
+  align-items: center;
+}
+
+.State {
+  display: inline-block;
+  padding: 5px 12px;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 20px;
+  text-align: center;
+  white-space: nowrap;
+  border-radius: 2em;
+}
+
+.octicon {
+  overflow: hidden;
+  vertical-align: text-bottom;
+  display: inline-block;
+  fill: currentColor;
+}
+</style>
--- src/components/OpenSourceWork.vue
+++ src/components/OpenSourceWork.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <h2 style="margin-bottom: 10px">Open-Source Contributions</h2>
-    <em style="font-size: 14px">I contribute wherever I can.</em>
+    <h2 style="margin: 40px">Open-Source Contributions</h2>
+    <FeaturedPR v-for="pr in prs" :pr="pr" :key="pr.project + pr.issueNum" />
   </div>
 </template>
+
+<script>
+import FeaturedPR from "./FeaturedPR.vue";
+
+export default {
+  data: () => {
+    return {
+      prs: [
+        {
+          project: "pytest-dev/pytest",
+          issueNum: 8319,
+          title: "Type annotation for request.param",
+        },
+        {
+          project: "pandas-dev/pandas",
+          issueNum: 37840,
+          title: "TST: Add test for setting item using python list (#19406)",
+        },
+        {
+          project: "sysid/sse-starlette",
+          issueNum: 3,
+          title: "Send pings as a custom `ping` event",
+        },
+      ],
+    };
+  },
+  components: { FeaturedPR },
+};
+</script>
