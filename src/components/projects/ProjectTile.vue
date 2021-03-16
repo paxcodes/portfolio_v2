7bff142e0a849dbf848d677ffc4fc5915cfac1bc
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-16 07:12:48 -0700

    feat: plug prop data in component
    Relevant issue: PFR-26

--- src/components/projects/ProjectTile.vue
+++ src/components/projects/ProjectTile.vue
@@ -1,17 +1,15 @@
 <template functional>
   <div>
     <h3>{{ props.project.title }}</h3>
+    
     <p>
-      Generates a report on the organization’s website, Facebook page, and
-      SoundCloud account activity by integrating with Google Analytics and
-      Facebook API; Data is stored in a JSON file so current week’s data can be
-      compared with last week’s. A web form is provided for the user to enter
-      SoundCloud data not available in their API.
+      {{ props.project.description }}
     </p>
     <ul>
-      <li>Screenshot 1</li>
-      <li>Screenshot 2</li>
-      <li>Screenshot 3</li>
+      <li v-for="image in props.project.screenshots" :key="image.src">
+        <img :src="image.src" :alt="image.alt" />
+        <span>{{ image.alt }}</span>
+      </li>
     </ul>
   </div>
 </template>
--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -11,7 +11,27 @@ import ProjectTile from "./ProjectTile.vue";
 export default {
   data: () => {
     return {
-      project: { title: "Weekly Roundup Generator" },
+      project: {
+        title: "Weekly Roundup Generator",
+        builtWith: ["Python", "Pytest", "PyInstaller", "Typer"],
+        description: `Generates a report on the organization’s website, Facebook page, and
+      SoundCloud account activity by integrating with Google Analytics and
+      Facebook API; Data is stored in a JSON file so current week’s data can be
+      compared with last week’s. A web form is provided for the user to enter
+      SoundCloud data not available in their API.`,
+        screenshots: [
+          {
+            src:
+              "https://margret.pw/static/cli_2-ee723e51b623486e11169d66a5d959b1.png",
+            alt: "CLI app",
+          },
+          {
+            src:
+              "https://margret.pw/static/test_a-081b8346c3d2a96231df28ecca86c213.png",
+            alt: "Tests without real HTTP calls",
+          },
+        ],
+      },
     };
   },
   components: { ProjectTile },
