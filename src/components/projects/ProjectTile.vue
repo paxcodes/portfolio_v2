950904fb1c38ddad8d3c7832c51e55de06058b03
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-18 07:07:20 -0700

    feat: list project's tech stack
    Relevant Issue: PFR-26

--- src/components/projects/ProjectTile.vue
+++ src/components/projects/ProjectTile.vue
@@ -1,7 +1,14 @@
 <template functional>
   <div>
     <h3>{{ props.project.title }}</h3>
-    
+    <ul>
+      <li
+        v-for="(data, key) in props.project.builtWith"
+        :key="props.project.title + key"
+      >
+        <slot name="built_with" :built-with="{ key, data }"></slot>
+      </li>
+    </ul>
     <p>
       {{ props.project.description }}
     </p>
--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,37 +1,58 @@
 <template>
   <div id="projects">
     <h2>Projects</h2>
-    <ProjectTile :project="project" />
+    <ProjectTile
+      v-for="project in projects"
+      :key="project.title"
+      :project="project"
+    >
+      <template v-slot:built_with="{ builtWith }">
+        <a v-if="builtWith.data.link" :href="builtWith.data.link">
+          <img
+            v-if="builtWith.data.icon_type"
+            :src="
+              require(`@/assets/built_with/${builtWith.key}.${builtWith.data.icon_type}`)
+            "
+            :alt="builtWith.key"
+            height="40"
+          />
+          <span v-else>{{ builtWith.key }}</span>
+        </a>
+      </template>
+    </ProjectTile>
   </div>
 </template>
 
 <script>
+import Project from "@/models/Project.js";
 import ProjectTile from "./ProjectTile.vue";
 
 export default {
   data: () => {
     return {
-      project: {
-        title: "Weekly Roundup Generator",
-        builtWith: ["Python", "Pytest", "PyInstaller", "Typer"],
-        description: `Generates a report on the organization’s website, Facebook page, and
+      projects: [
+        new Project({
+          title: "Weekly Roundup Generator",
+          builtWith: ["Python", "Pytest", "PyInstaller", "Typer"],
+          description: `Generates a report on the organization’s website, Facebook page, and
       SoundCloud account activity by integrating with Google Analytics and
       Facebook API; Data is stored in a JSON file so current week’s data can be
       compared with last week’s. A web form is provided for the user to enter
       SoundCloud data not available in their API.`,
-        screenshots: [
-          {
-            src:
-              "https://margret.pw/static/cli_2-ee723e51b623486e11169d66a5d959b1.png",
-            alt: "CLI app",
-          },
-          {
-            src:
-              "https://margret.pw/static/test_a-081b8346c3d2a96231df28ecca86c213.png",
-            alt: "Tests without real HTTP calls",
-          },
-        ],
-      },
+          screenshots: [
+            {
+              src:
+                "https://margret.pw/static/cli_2-ee723e51b623486e11169d66a5d959b1.png",
+              alt: "CLI app",
+            },
+            {
+              src:
+                "https://margret.pw/static/test_a-081b8346c3d2a96231df28ecca86c213.png",
+              alt: "Tests without real HTTP calls",
+            },
+          ],
+        }),
+      ],
     };
   },
   components: { ProjectTile },
