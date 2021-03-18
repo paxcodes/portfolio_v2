b96d264945f9f70458c118acccc7c9020098acbf
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-18 14:21:01 -0700

    feat: add contribIcon data

--- src/components/oss/OpenSourceWork.vue
+++ src/components/oss/OpenSourceWork.vue
@@ -27,28 +27,33 @@ export default {
           issueNum: 8319,
           link: "https://github.com/pytest-dev/pytest/pull/8319",
           title: "Type annotation for request.param",
+          contribIcon: ["Python"]
         },
         {
           project: "pandas-dev/pandas",
           issueNum: 37840,
           link: "https://github.com/pandas-dev/pandas/pull/37840",
           title: "TST: Add test for setting item using python list (#19406)",
+          contribIcon: ["Python"]
         },
         {
           project: "sysid/sse-starlette",
           issueNum: 3,
           link: "https://github.com/sysid/sse-starlette/pull/3",
           title: "Send pings as a custom `ping` event",
+          contribIcon: ["Python"]
         },
       ],
       other_prs: [
         {
           count: 5,
           project: "topanswers/topanswers",
+          contribIcon: ["JS", "Docker"]
         },
         {
           count: 4,
           project: "Vancouver-Datajam/project_4",
+          mainLang: "Python"
         },
       ],
     };
