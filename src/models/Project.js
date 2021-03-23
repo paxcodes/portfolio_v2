aa9f645933edf895174507b60b8fee72842c267f
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-03-22 19:00:17 -0700

    feat: load static files from the API
    Closes PFR-44

--- src/models/Project.js
+++ src/models/Project.js
@@ -5,9 +5,16 @@ export default class Project {
         this.title = title;
         this.builtWith = this._transformBuiltWith(builtWith);
         this.description = description;
-        this.screenshots = screenshots;
+        this.screenshots = this._transformScreenshots(screenshots);
+    }
+    _transformScreenshots(screenshots) {
+        return screenshots.map(value => ({
+            alt: value.alt,
+            src: `${process.env.VUE_APP_API_URL}/${value.src}`
+        }));
     }
 
+    screenshots
     _transformBuiltWith(builtWith) {
         const reducer = (currentData, builtWithKey) => {
             currentData[builtWithKey] = BUILT_WITH[builtWithKey]
--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+    baseURL: process.env.VUE_APP_API_URL + '/v1',
     withCredentials: false,
     headers: {
         "Accept": "application/json",
