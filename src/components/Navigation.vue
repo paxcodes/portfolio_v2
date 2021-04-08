70eec5bf870294ccddc2095d14491e06281591ef
Margret `Pax` Williams <pax.margret@tutanota.com>
2021-04-07 18:47:54 -0700

    fix: make html more semantic

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,32 +1,30 @@
 <template functional>
-  <div id="navigation">
-    <ul>
-      <li><a href="#projects">Projects</a></li>
-      <li><a href="#os-contributions">Open-Source Contributions</a></li>
-      <li><a href="#education">Education</a></li>
-      <li>
-        <a href="mailto:pax.margret@tutanota.com"
-          ><img
-            style="background: white"
-            :src="require('@/assets/email.svg')"
-            alt="email me"
-            height="20"
-        /></a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/paxcodes"
-          target="_blank"
-          rel="noopener noreferrer"
-          ><img
-            style="background: black"
-            :src="require('@/assets/github-light.png')"
-            alt="my github"
-            height="32"
-        /></a>
-      </li>
-    </ul>
-  </div>
+  <nav id="navigation">
+    <div id="menu-links">
+      <a href="#projects">Projects</a>
+      <a href="#os-contributions">Open-Source Contributions</a>
+      <a href="#education">Education</a>
+
+      <a href="mailto:pax.margret@tutanota.com"
+        ><img
+          style="background: white"
+          :src="require('@/assets/email.svg')"
+          alt="email me"
+          height="20"
+      /></a>
+
+      <a
+        href="https://github.com/paxcodes"
+        target="_blank"
+        rel="noopener noreferrer"
+        ><img
+          style="background: black"
+          :src="require('@/assets/github-light.png')"
+          alt="my github"
+          height="32"
+      /></a>
+    </div>
+  </nav>
 </template>
 
 <style>
@@ -37,14 +35,7 @@
   text-align: center;
 }
 
-#navigation a {
-  color: whitesmoke;
-  font-weight: bold;
-  display: inline-block;
-  padding: 1rem;
-}
-
-#navigation > ul {
+#navigation > #menu-links {
   padding: 0;
   margin: 0;
   list-style-type: none;
@@ -52,9 +43,12 @@
   justify-content: center;
 }
 
-#navigation > ul > li {
+#navigation > #menu-links > a {
   display: flex;
   align-items: center;
   margin: 0 1rem;
+  color: whitesmoke;
+  font-weight: bold;
+  padding: 1rem;
 }
 </style>
